af1d670f6a19590bece16e22d575c93b64f88296
ravishashu <56905495+ravishashu@users.noreply.github.com>
2026-04-28 10:43:34 +0100

    added serilog and seq docker

--- ConferenceAttendees.API/Program.cs
+++ ConferenceAttendees.API/Program.cs
@@ -1,11 +1,20 @@
 using ConferenceAttendees.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("ConferenceAttendeeDatabaseConnection");
 // Add services to the container.
 builder.Services.AddDbContext<ApplicationDbContext>(options => {
     options.UseSqlServer(connectionString);
+});
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext();
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
